442c07d037956e540d21263ea47225b9400de164
Rafael54x <rafael.natanael46@gmail.com>
2026-02-10 09:00:39 +0700

    Update Inherit

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,6 @@ from . import peepl_weekly_report_bod
 from . import peepl_weekly_report_department
 from . import hr_department
 from . import peepl_department_view
-from . import res_config_settings
 from . import peepl_user_assignment
 from . import peepl_pic_overview
 from . import res_users
